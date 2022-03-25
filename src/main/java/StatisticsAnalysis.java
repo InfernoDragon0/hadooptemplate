891f9d00cd74600b0501fe181e37e617a002f70d
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-03-25 11:46:39 +0800

    Lab 9

--- src/main/java/StatisticsAnalysis.java
+++ src/main/java/StatisticsAnalysis.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.log4j.BasicConfigurator;
 
-import java.io.IOException;
 import java.util.Date;
 
 public class StatisticsAnalysis {
@@ -15,6 +14,8 @@ public class StatisticsAnalysis {
         BasicConfigurator.configure();
 
         Configuration conf = new Configuration();
+        //conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        conf.set("fs.yarn.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
         Job job = Job.getInstance(conf, "StatisticsAnalysis");
 
         job.setJarByClass(StatisticsAnalysis.class);
@@ -24,8 +25,8 @@ public class StatisticsAnalysis {
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        Path inputPath = new Path("input");
-        Path outputPath = new Path("output" + new Date().getTime());
+        Path inputPath = new Path("hdfs://localhost:19000/input");
+        Path outputPath = new Path("hdfs://localhost:19000/output" + new Date().getTime());
 
         FileInputFormat.addInputPath(job, inputPath);
         FileOutputFormat.setOutputPath(job, outputPath);
